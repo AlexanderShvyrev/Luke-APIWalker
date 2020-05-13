34f46f1a38d4977e13d59c27cc0f10498804b996
Alexander Shvyrev <shurik_stv@yahoo.com>
2020-05-13 17:10:46 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/sketchy/bootstrap.min.css';
 import './App.css';
+import { Router, navigate } from '@reach/router';
+import Main from './components/Main';
+import Action from './components/Action';
+
 
 function App() {
+  const clickHandler = e => {
+    navigate("/")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="jumbotron">
+        <h1 className="display-3">Hello, stranger!</h1>
+        <p className="lead">Welcome to Star Wars World.</p>
+        <hr className="my-5"></hr>
+        <p>Find everything you need for your journey</p>
+        <input className="btn btn-lg btn-info" value="Home" onClick={clickHandler} />
+
+      </div>
+      <Router>
+        <Main exact path="/" />
+        <Action exact path="/:category/:id" component={Action} />
+      </Router>
+    </Fragment>
   );
 }
 
